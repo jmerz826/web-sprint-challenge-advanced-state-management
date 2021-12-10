33db227ffaefe36fffb0ea72d39135128f4c576d
John Merz <jmerz826@gmail.com>
2021-12-10 14:50:29 -0500

    thunk action complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
@@ -6,9 +6,17 @@ import Header from './components/Header';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import { connect } from "react-redux";
 
-const App = () => {
-  console.log('initial commit/setup');
+import { fetchSmurfs } from "./actions";
+
+
+const App = (props) => {
+
+  useEffect(() => {
+    props.fetchSmurfs();
+  }, []);
+
   return (
     <div className="App">
       <Header />
@@ -21,7 +29,13 @@ const App = () => {
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs
+  }
+}
+
+export default connect(mapStateToProps, {fetchSmurfs})(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,19 @@ export const FETCH_FAILURE = 'FETCH_FAILURE';
 export const ADD_SMURF = 'ADD_SMURF';
 export const ADD_ERROR = 'ADD_ERROR';
 
-export const fetchSmurfs = dispatch => {
-
+export const fetchSmurfs = () => {
+    return (dispatch) => {
+        dispatch({ type: FETCH_START });
+    
+        axios.get('http://localhost:3333/smurfs')
+        .then(res => {
+            dispatch({type:FETCH_SUCCESS, payload:res.data})
+        })
+        .catch(err => {
+            console.error(err);
+            dispatch({ type: FETCH_FAILURE, payload:err });
+        })
+    }
 }
 
 //Task List:
